8c3f4fdd475c7fb5c6c69bc67a500ff6f752e683
Alex Torres <almtorres805@gmail.com>
2023-04-03 16:25:39 -0700

    skeleton code for project

---
+++ Project0/sgrep.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int charc, char *argv [])
+{
+        if (charc < 3){
+                printf("Usage: %s [-n] [-c] [-h] [-p PATTERN]... FILE...\n", argv[0]);
+        } else {
+
+        }
+        return 0;
+}
